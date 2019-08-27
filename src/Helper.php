ae62e782934a2c9510b71dc97301f681b46e6203
gaazok <chenxi2511@qq.com>
2019-08-27 23:30:47 +0800

    调整辅助函数

--- src/Helper.php
+++ src/Helper.php
@@ -2,60 +2,14 @@
 
 use Jeekens\Container\Container;
 
-if (!function_exists('target')) {
+if (!function_exists('container')) {
     /**
-     * 从容器中取出目标服务
+     * 获取容器
      *
-     * @param string $abstract
-     * @param array $parameters
-     *
-     * @return mixed|object
-     *
-     * @throws ReflectionException
-     * @throws \Jeekens\Container\Exception\BindingResolutionException
-     * @throws \Jeekens\Container\Exception\EntryNotFoundException
-     * @throws \Jeekens\Container\Exception\NotInstantiableException
+     * @return Container
      */
-    function target(string $abstract, array $parameters = [])
+    function container()
     {
-        return Container::getInstance()->make($abstract, $parameters);
-    }
-}
-
-if (!function_exists('injection')) {
-    /**
-     * 向容器中注入一个服务
-     *
-     * @param string $abstract
-     * @param null $concrete
-     * @param bool $shared
-     *
-     * @throws ReflectionException
-     * @throws \Jeekens\Container\Exception\BindingResolutionException
-     * @throws \Jeekens\Container\Exception\EntryNotFoundException
-     * @throws \Jeekens\Container\Exception\NotInstantiableException
-     */
-    function injection(string $abstract, $concrete = null, $shared = false)
-    {
-        Container::getInstance()->bind($abstract, $concrete, $shared);
-    }
-}
-
-if (!function_exists('injection_share')) {
-    /**
-     * 向容器中注入一个共享服务(单例)
-     *
-     * @param string $abstract
-     * @param null $concrete
-     * @param bool $shared
-     *
-     * @throws ReflectionException
-     * @throws \Jeekens\Container\Exception\BindingResolutionException
-     * @throws \Jeekens\Container\Exception\EntryNotFoundException
-     * @throws \Jeekens\Container\Exception\NotInstantiableException
-     */
-    function injection_share(string $abstract, $concrete = null)
-    {
-        Container::getInstance()->bind($abstract, $concrete, true);
+        return Container::getInstance();
     }
 }
